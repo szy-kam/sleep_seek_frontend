0faa47256953942d76db7ab73c969fc19f91c989
unknown <300755@uwr.edu.pl>
2020-10-27 23:21:15 +0100

    stays card - added loadMore

--- src/components/Stays/stays.js
+++ src/components/Stays/stays.js
@@ -1,27 +1,14 @@
 
 import React, {Component} from 'react';
-
-import {StaysCardRepository} from '../../repository/Stays'
 import StaysCard from '../widgets/StaysCards/staysCard'
 
 class Stays extends Component{
-
-    state = {
-        stays: []
-    }
-    
-
-    componentDidMount() {
-        StaysCardRepository().then(response => this.setState({ stays: response}))
-    }
-
-
 
     render(){
         return (
             <div>
                 Stays
-                <StaysCard stays={this.state.stays} />
+                <StaysCard template="default"/>
             </div>
         )
     }
--- src/components/widgets/StaysCards/staysCard.js
+++ src/components/widgets/StaysCards/staysCard.js
@@ -1,32 +1,78 @@
-import React from 'react';
+import { template } from '@babel/core';
+import React, {Component} from 'react';
 import { Link } from 'react-router-dom';
-
+import {StaysCardRepository} from '../../../repository/Stays'
 import style from './staysCard.css'
 
-const StaysCard = (props) => {
 
-    return props.stays.map((item,i) =>(
-        <div key={i} className={style.staysCard}>
-            <div className={style.left}>
-                <div className={style.image}><Link to={`/stays/${item.id}`}><img src="https://picsum.photos/150.jpg" alt=""></img></Link></div>
-            </div>
-            <div className={style.right}>
-                <div className={style.name}><Link to={`/stays/${item.id}`}>{item.name}</Link></div>
-                <div className={style.address}>Address: {item.address.city}, {item.address.address}</div>
-                <div className={style.price}>{item.address.zipCode} zł</div>
-                <button className={style.itemButton}><Link to={`/stays/${item.id}`}>View more</Link></button>
-            </div>
-        </div>
+class StaysCard extends Component {
 
-    ))
-    // return (
-    //     <div>
-    //         {console.log(props)}
-    //         "alamakota"
-    //     </div>
-        
-    // )
+    state = {
+        stays: []
+    }
+
+    componentDidMount() {
+        StaysCardRepository().then(response => this.setState({ stays: response}))
+    }
     
+    renderMoreHandler=() =>{
+        StaysCardRepository().then(response => this.setState({ stays: [...this.state.stays,...response]}))
+    }
+    
+    renderMore(){
+        return <button className={style.showMoreButton} onClick={this.renderMoreHandler}>Show more</button>
+    }
+
+
+    renderCards(template, stays){
+        switch(template){
+            case('default'):
+            return (
+                <div className={style.staysContainerT1}>
+                {stays.map((item,i) =>(
+                    <div key={i} className={style.staysCardT1}>
+                        <div className={style.left}>
+                            <div className={style.image}><Link to={`/stays/${item.id}`}><img src={`https://picsum.photos/15${i}.jpg`} alt=""></img></Link></div>
+                        </div>
+                        <div className={style.right}>
+                            <div className={style.name}><Link to={`/stays/${item.id}`}>{item.name}</Link></div>
+                            <div className={style.address}>Address: {item.address.city}, {item.address.address}</div>
+                            <div className={style.price}>{item.address.zipCode} zł</div>
+                            <button className={style.itemButton}><Link to={`/stays/${item.id}`}>View more</Link></button>
+                        </div>
+                    </div>
+                ))}
+            </div>)
+            break;
+            case('mini'):
+                return (
+                    <div className={style.staysContainerT2}>
+                    {stays.map((item,i) =>(
+                    <div key={i} className={style.staysCardT2}>
+                        <div className={style.image}><Link to={`/stays/${item.id}`}><img src={`https://picsum.photos/15${i}.jpg`} alt=""></img></Link></div>
+                        <div className={style.name}><Link to={`/stays/${item.id}`}>{item.name}</Link></div>
+                        <div className={style.price}>{item.address.zipCode} zł</div>
+                        <button className={style.itemButton}><Link to={`/stays/${item.id}`}>View more</Link></button>
+                    </div>
+                ))}
+                </div>)
+            break;
+            default:
+                return <p>No card template choosen.</p>
+        }
+       
+    }
+    
+
+    
+    render(){
+        return (
+        <div >
+            {this.renderCards(this.props.template, this.state.stays)}
+            {this.renderMore()}
+        </div>
+        )
+    }
     
 }
 
